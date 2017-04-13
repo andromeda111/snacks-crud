ff94d0ea3768d325a85668163017a5753ee6a4fb
John <jmseymour111@gmail.com>
2017-04-13 10:35:34 -0700

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,15 @@ const path = require('path')
      seeds: {
        directory: path.join(__dirname, 'db', 'seeds')
      }
+   },
+   production: {
+     client: 'pg',
+     connection: process.env.DATABASE_URL,
+     migrations: {
+       directory: path.join(__dirname, 'db', 'migrations')
+     },
+     seeds: {
+       directory: path.join(__dirname, 'db', 'seeds')
+     }
    }
  }
